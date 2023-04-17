1a67a60449df418c5d9cc5696a8896d4619b4c3a
Fabrício Ferreira da Silva <fabricio.thyala@gmail.com>
2023-04-17 09:45:31 -0300

    Change readme

--- lib/app/widgets/custom_dialog_task.dart
+++ lib/app/widgets/custom_dialog_task.dart
@@ -11,58 +11,60 @@ class CustomDialogTask extends GetWidget<HomeController> {
           color: Colors.white,
           margin: const EdgeInsets.all(20),
           padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                'Criar Tarefa',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Criar Tarefa',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: controller.titleTaskController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Título',
+                const SizedBox(height: 20),
+                TextField(
+                  controller: controller.titleTaskController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Título',
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: controller.descriptionTaskController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Descrição',
+                const SizedBox(height: 20),
+                TextField(
+                  controller: controller.descriptionTaskController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Descrição',
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Obx(
-                () => CheckboxListTile(
-                  value: controller.check.value,
-                  onChanged: (value) {
-                    controller.check.toggle();
-                    controller.update();
+                const SizedBox(height: 20),
+                Obx(
+                  () => CheckboxListTile(
+                    value: controller.check.value,
+                    onChanged: (value) {
+                      controller.check.toggle();
+                      controller.update();
+                    },
+                    activeColor: Colors.blue.shade900,
+                    dense: true,
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 2),
+                    title: const Text('Concluída', style: TextStyle(fontSize: 16)),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    controller.addTask();
+                    Get.back();
                   },
-                  activeColor: Colors.blue.shade900,
-                  dense: true,
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 2),
-                  title: const Text('Concluída', style: TextStyle(fontSize: 16)),
+                  child: const Text('CRIAR TAREFA'),
                 ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  controller.addTask();
-                  Get.back();
-                },
-                child: const Text('CRIAR TAREFA'),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       );
